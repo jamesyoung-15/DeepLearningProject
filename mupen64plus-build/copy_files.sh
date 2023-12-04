9c0efc6885f776c8dcca538f69956f130c055c18
jamesyoung <jamesyoung3931@gmail.com>
2023-12-04 20:49:49 +0800

    cleanup

---
+++ mupen64plus-build/copy_files.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd test/ && cp mupen64plus-audio-sdl.so mupen64plus.ini  mupen64plus-input-sdl.so mupen64plus-rsp-hle.so \
+ mupen64plus-video-glide64mk2.so mupen64plus-video-rice.so mupencheat.txt libmupen64plus.so.2  libmupen64plus.so.2.0.0 \
+ Glide64mk2.ini InputAutoCfg.ini ../../gym_mariokart64/m64py
